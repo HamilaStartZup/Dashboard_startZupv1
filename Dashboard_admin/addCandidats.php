8a2e3bb0cc3308fbdbec756fd5b7904c6b6bd9c0
ANAS EL KHIAT <anas@adame-techs.com>
2023-11-15 16:18:10 +0100

    lien vers listeAppel.php

--- Dashboard_admin/addCandidats.php
+++ Dashboard_admin/addCandidats.php
@@ -232,11 +232,12 @@ button:hover {
         ><i class="fas fa-calendar fa-fw me-3"></i
        ><span>Présence</span></a
        >
-     <a
-        href="#"
-        class="list-group-item list-group-item-action py-2 ripple"
-        ><i class="fas fa-users fa-fw me-3"></i><span>Users</span></a
-       >
+      <a
+        href="listeAppels.php"
+        class="list-group-item list-group-item-action py-2 ripple ripple active"
+        ><i class="fa-sharp fa-solid fa-list me-3"></i>
+        <span>Liste d'appels</span></a
+      >
      <a
      href="../logout.php"
         class="list-group-item list-group-item-action py-2 ripple"
