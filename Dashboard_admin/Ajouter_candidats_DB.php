a350413622d1ff14d3815580f608b567ad4fbe93
Hamila@1992 <halima.jounaidi@startzup.onmicrosoft.com>
2023-11-23 16:03:08 +0100

    ADD SKILLS COMMIT

--- Dashboard_admin/Ajouter_candidats_DB.php
+++ Dashboard_admin/Ajouter_candidats_DB.php
@@ -2,8 +2,7 @@
 session_start();
         require('../config.php');
       
-       
-       if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $Nom = $_POST['Nom'];
             $Prenom = $_POST['Prenom'];
             $Tel = $_POST['Tel'];
@@ -13,31 +12,37 @@ session_start();
            $Designation=$_POST['designation'];
            $Disponibility=$_POST['disponibility'];
            $code_profile='SZ_'.rand(100, 900);
-       
+     
            $competences =$_POST['ary'];
-           $C=$competences[0];
-           for($i=1;$i<count($competences);$i++){
-              $C.= ','.$competences[$i];
-           }
-           echo'hi'. var_dump($competences);
-           // $filename = $_FILES["uploadfile"]["name"];
-            //$tempname = $_FILES["uploadfile"]["tmp_name"];
-           // $folder = "./image/" . $filename;
 
             // Verification de la présence dans la DB avec l'email
-           $sql_verif = "SELECT COUNT(*) from student WHERE email = '$Email'";
+         $sql_verif = "SELECT COUNT(*) from student WHERE email = '$Email'";
             $verif = $conn->prepare($sql_verif);
             $verif->execute();
 
             // Insertion dans DB
             if ($verif->fetchColumn() == 0) {
               
-                    $sql = " INSERT INTO student (nom, Prenom, phone, adresse, email,date_naissance,disponibility,designation,code_profile,competence) VALUES ('$Nom', '$Prenom','$Tel', '$Adresse', '$Email', '$Birthday','$Disponibility','$Designation','$code_profile','$C')";
+                    $sql = " INSERT INTO student (nom, Prenom, phone, adresse, email,date_naissance,disponibility,designation,code_profile) VALUES ('$Nom', '$Prenom','$Tel', '$Adresse', '$Email', '$Birthday','$Disponibility','$Designation','$code_profile')";
                     $conn->exec($sql);
-             echo '<script> alert("Le candidat a été ajouté avec succès.");
-                   location.replace("addCandidats.php");
-</script>';
-                
+                   $lastIdSql= "SELECT `id`FROM `student` ORDER BY `reg_date` DESC LIMIT 1";
+                   //GET LAST STUDENT ID 
+                   $lastIdCon= $conn->prepare($lastIdSql);
+                   $lastIdCon->execute();
+                   $resultlastId=$lastIdCon->fetch(PDO::FETCH_ASSOC);
+                   for ($x = 0; $x <= count($competences)-1; $x++) {
+                     //INSERT KILLS WITH  USERS  ID 
+                     $skills ="INSERT INTO `student_skills`( `id_student`, `id_skills`) VALUES ('$resultlastId[id]','$competences[$x]')";
+                     $conn->exec($skills);
+                   }
+                  
+                  
+   
+                   echo'jddjeje'.$resultlastId['id'];
+            // echo '<script> alert("Le candidat a été ajouté avec succès.");
+                   //location.replace("addCandidats.php");
+//</script>';
+                     
                   
                 
             } else {
@@ -48,7 +53,6 @@ session_start();
         }
    
    
-   
-        
+       
         ?>
 
