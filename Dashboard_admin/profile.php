66927ac7ec454031789ae4ef68ea3f9e76c92cc9
Anas7823 <anaselkhiat78@gmail.com>
2023-12-01 14:55:10 +0100

    lien vers la page de modif

--- Dashboard_admin/profile.php
+++ Dashboard_admin/profile.php
@@ -218,7 +218,28 @@
     <!-- Sidebar -->
   
     <!-- Navbar -->
- 
+    <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+        <!-- Container wrapper -->
+        <div class="container-fluid">
+            <!-- Toggle button -->
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#sidebarMenu"
+                    aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+
+            <!-- Brand -->
+            <a class="navbar-brand" href="#">
+                <img src="../images/icon.png" height="25" alt="KJJJ"/>
+            </a>
+            <!-- Search form -->
+            <form class="d-none d-md-flex input-group w-auto my-auto">
+                <input autocomplete="off" type="search" class="form-control rounded"
+                       placeholder='Search (ctrl + "/" to focus)' style="min-width: 225px"/>
+                <span class="input-group-text border-0"><i class="fas fa-search"></i></span>
+            </form>
+        </div>
+        <!-- Container wrapper -->
+    </nav>
     <!-- Navbar -->
   </header>
   <!--Main Navigation-->
@@ -228,7 +249,10 @@
     <div class="container">
       <div class="row">
         <div class="col">
-          <a class="btn btn-info" href="./edit-profil.php">Modifier</a>
+          <?php 
+          $url = "./edit-profil.php?id=$Profile[id]";
+          echo "<a class='btn btn-info' href='$url'>Modifier</a>"
+          ?>
         </div>
         <div class="col">
           
